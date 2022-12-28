d6a59fa5a069408dac80f32b9212fefa7c67b42b
Marasy Phi <hipporasy@gmail.com>
2022-12-28 16:01:54 +1100

    Initial Commit

---
+++ Steam/SteamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteamApp.swift
+//  Steam
+//
+//  Created by Marasy Phi on 28/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SteamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
